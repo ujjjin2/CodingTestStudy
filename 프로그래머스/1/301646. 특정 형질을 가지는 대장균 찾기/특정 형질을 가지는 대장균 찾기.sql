880c6bebdd9c149b883cdbd54a615639de32af52
허유진 <93508031+ujjjin2@users.noreply.github.com>
2025-01-07 15:18:48 +0900

    Update 특정 형질을 가지는 대장균 찾기.sql

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -3,5 +3,5 @@ FROM ECOLI_DATA d
 WHERE (d.GENOTYPE & 2) = 0
   AND (
     (d.GENOTYPE & 4) = 4 -- 3이 포함되어야 함
-    OR (d.GENOTYPE & 1) = 1 -- 4가 포함되어야 함
+    OR (d.GENOTYPE & 1) = 1 -- 1이 포함되어야 함
   )
