82e49d9f6f09898688a1fa15b1399aa137ed4ee7
허유진 <93508031+ujjjin2@users.noreply.github.com>
2025-01-07 15:36:48 +0900

    Update 부모의 형질을 모두 가지는 대장균 찾기.sql

--- 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -2,7 +2,7 @@ select
     d.ID,
     d.GENOTYPE,
     e.GENOTYPE as PARENT_GENOTYPE
-from ECOLI_DATA d
-inner join ECOLI_DATA e on e.ID = d.PARENT_ID
+from ECOLI_DATA d --자식테이블
+inner join ECOLI_DATA e on e.ID = d.PARENT_ID -- 부모테이블
 where d.GENOTYPE & e.GENOTYPE = e.GENOTYPE
 order by d.ID asc
